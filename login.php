d1a7b8172498f7dd597dbe9e327fb929c8f97915
IdkMyNaam <127683838+IdkMyNaam@users.noreply.github.com>
2025-04-02 13:38:54 +0200

    Login Main

--- login.php
+++ login.php
@@ -2,7 +2,33 @@
 require_once ('head.php');
 require_once ('header.php');
 ?>
+<main>
 
+</main>
+<div class="container home">
 
+<?php
+if (isset($_GET['msg'])) {
+    echo "<div class='msg'>" . $_GET['msg'] . "</div>";
+}
+if (isset($_GET['error'])) {
+    echo "<div class='msg'>" . $_GET['error'] . "</div>";
+}
+?>
+
+<form action="backend/loginController.php" method="POST">
+    <div class="form-group">
+        <label for="username">Gebruikersnaam</label>
+        <input type="text" name="username" id="username" placeholder="Gebruikersnaam">
+    </div>
+    <div class="form-group">
+        <label for="password">Wachtwoord</label>
+        <input type="password" name="password" id="password" placeholder="Wachtwoord">
+    </div>
+
+    <input type="submit" value="Inloggen">
+</form>
+
+</div>
 </body>
 </html>
