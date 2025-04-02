eba6201f04b3ee651f08dac59eaa4b3874bc49be
IdkMyNaam <127683838+IdkMyNaam@users.noreply.github.com>
2025-04-02 13:36:05 +0200

    f

--- head.php
+++ head.php
@@ -6,3 +6,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php $base_url?>/css/style.css">
     <title>Document</title>
+</head>
+<body>
