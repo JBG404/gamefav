8d2bfd9a856ddfe4d25a333c63590394f7ba0f56
jbgamingnl <jake@jbgaming.nl>
2025-04-02 15:15:24 +0200

    home.

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 
    <nav>
     <a href="<?php  $base_url; ?>/gamesfavs/index.php">|Games List|</a>
+    <a href="<?php $base_url; ?>/index.php">|home|</a>
    </nav> 
    <h1>FAVOURITE GAMES LIST</h1>
    <nav id="login">
