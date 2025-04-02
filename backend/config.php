d01733b9a8ed19062ec2add5718b44b38220ed8c
IdkMyNaam <127683838+IdkMyNaam@users.noreply.github.com>
2025-04-02 13:32:27 +0200

    extra

--- backend/config.php
+++ backend/config.php
@@ -7,7 +7,7 @@
 //Deze config wordt hierna _niet_ meegestuurd naar je groepsgenoten. Zo kan iedereen zijn eigen wachtwoord, etc. invullen.
 
 $dbHost = 'localhost';
-$dbName = 'gamefav';
+$dbName = 'favgames';
 $dbUser = 'root';
 $dbPass = '';
 
---
+++ head.php
@@ -0,0 +1,8 @@
+<?php session_start() ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php $base_url?>/css/style.css">
+    <title>Document</title>
