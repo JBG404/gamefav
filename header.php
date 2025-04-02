870b408cdc3c3724d3901a812c3da335264307f7
jbgamingnl <jake@jbgaming.nl>
2025-04-02 15:11:46 +0200

    header/headerstyle

--- header.php
+++ header.php
@@ -1,3 +1,10 @@
 <header>
-    
+
+   <nav>
+    <a href="<?php  $base_url; ?>/gamesfavs/index.php">|Games List|</a>
+   </nav> 
+   <h1>FAVOURITE GAMES LIST</h1>
+   <nav id="login">
+      <a href="<?php  $base_url; ?>/login.php">|Login|</a>
+   </nav>
 </header>
